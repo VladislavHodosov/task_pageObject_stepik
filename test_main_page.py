4d4bfc0af0243d1f1f0b4fea146ce6e402e63460
vhodosov <78086257+VladislavHodosov@users.noreply.github.com>
2023-03-24 17:43:50 +0400

    add new test case/asser methods in mainPage class

--- test_main_page.py
+++ test_main_page.py
@@ -1,13 +1,19 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-
+from .pages.main_page import MainPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
 # link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 # def test_that_add_basket_button_present(browser):
 #     browser.get(link)
